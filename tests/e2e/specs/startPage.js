2d84313026cdcdf3a106a394172a8f7bd9b2ce54
Andreas Cederström <andreas.cederstrom@factor10.com>
2019-11-06 14:58:29 +0100

    Renamed header test

--- tests/e2e/specs/startPage.js
+++ tests/e2e/specs/startPage.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Start page', () => {
-  it('shall contain a welcoming header', () => {
+  it('contains a welcoming header', () => {
     cy.visit('/');
     cy.contains('h1', 'Welcome');
   });
