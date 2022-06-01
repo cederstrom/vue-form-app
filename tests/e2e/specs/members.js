a0bf0e146606182cfca520662af90a409a0488d8
Andreas Cederström <andreas.cederstrom@factor10.com>
2022-06-02 00:41:44 +0200

    Prettier formatting

--- tests/e2e/specs/members.js
+++ tests/e2e/specs/members.js
@@ -1,22 +1,22 @@
 /// <reference types="Cypress" />
 
-describe('Member page', () => {
+describe("Member page", () => {
   beforeEach(() => {
-    cy.server()
+    cy.server();
     cy.route({
-      method: 'GET',
-      url: '/members',
-      response: [{ name: 'Kevin', age: 12 }, { name: 'Cortez', age: 56 }]
-    })
-    cy.visit('/');
-    cy.contains('Members').click();
+      method: "GET",
+      url: "/members",
+      response: [{ name: "Kevin", age: 12 }, { name: "Cortez", age: 56 }]
+    });
+    cy.visit("/");
+    cy.contains("Members").click();
   });
 
-  it('contains some initial data', () => {
-    cy.get('h1').contains('Members');
-    cy.get('[testid="members"] tr:first').contains('Kevin');
-    cy.get('[testid="members"] tr:first').contains('12');
-    cy.get('[testid="members"] tr:nth-child(2)').contains('Cortez');
-    cy.get('[testid="members"] tr:nth-child(2)').contains('56');
+  it("contains some initial data", () => {
+    cy.get("h1").contains("Members");
+    cy.get('[testid="members"] tr:first').contains("Kevin");
+    cy.get('[testid="members"] tr:first').contains("12");
+    cy.get('[testid="members"] tr:nth-child(2)').contains("Cortez");
+    cy.get('[testid="members"] tr:nth-child(2)').contains("56");
   });
 });
