3dad613393fbcaf86a0234e8345181c67bd2fde9
Andreas Cederström <andreas.cederstrom@factor10.com>
2022-10-14 07:46:25 +0200

    Specify cypress output folder

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,12 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  fixturesFolder: 'tests/e2e/fixtures',
-  screenshotsFolder: 'tests/e2e/screenshots',
-  videosFolder: 'tests/e2e/videos',
+  fixturesFolder: "tests/e2e/fixtures",
+  screenshotsFolder: "tests/e2e/output/screenshots",
+  videosFolder: "tests/e2e/output/videos",
   e2e: {
-    baseUrl: 'http://localhost:8080',
-    specPattern: 'tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'tests/e2e/support/index.js',
+    baseUrl: "http://localhost:8080",
+    specPattern: "tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "tests/e2e/support/index.js",
   },
-})
+});
